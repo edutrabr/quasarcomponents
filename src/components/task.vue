0b0354dc7b45309b2192cd87c476cac3bb30a4b3
edutrabr <elmariodutra@hotmail.com>
2024-04-09 21:29:14 -0300

    Initial commit

---
+++ src/components/task.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <li>
+    <div>{{task.name}} [{{task.id}}]</div>
+    <small>{{task.dueDate}} @ {{task.dueTime}}</small>
+    <button @click="deleteTask(task.id)">X</button>
+  </li>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  props: {
+    task: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-page>
+    <ul>
+      <task v-for="(task, index) in tasks"
+        :task="task"
+        :key="index"
+      >
+      </task>
+    </ul>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import Task from 'components/task.vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { Task },
+  data() {
+    return {
+      tasks: [
+        {
+          'id': 1,
+          'name': 'Comprar bananas',
+          'dueDate': '10/04/2024',
+          'dueTime' : '18:00'
+        },
+        {
+          'id': 2,
+          'name': 'Ir ao shopping',
+          'dueDate': '10/04/2024',
+          'dueTime' : '19:00'
+        },
+        {
+          'id': 3,
+          'name': 'Code Vue',
+          'dueDate': '10/04/2024',
+          'dueTime' : '22:00'
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1)
+    }
+  }
+})
+</script>
